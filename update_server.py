ab071375bea7aad71c04f6104798819f8bfbe0e0
Daniel <daniel.pakula102@gmail.com>
2017-05-24 17:00:34 +0300

    Add some server functionality

--- update_server.py
+++ update_server.py
@@ -1,17 +1,21 @@
 import threading
 import ssl
+import SocketServer
 RECEIVE_SIZE = 1024
+SERVER_IP_AND_PORT = '127.0.0.1'
 
 
+class UpdateServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass  # No need to override anything, all gucci
 
 
-
-
-
-
-
-
+class UpdateServerRequestHandler(SocketServer.StreamRequestHandler):
+    """
+    Handles one connection to a client
+    """
+    def handle(self):
+        print "connection from %s" % self.client_address[0]
 
 
 if __name__ == "__main__":
-    me()  # debug
+    pass
